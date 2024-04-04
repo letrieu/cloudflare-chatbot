9d7a2274793a6d751ce8eb0d4ab0a08da2f68301
trieuld <trieuld@vng.com.vn>
2024-04-04 19:51:53 +0700

    Add vercel ai, cloudflare ai

--- env.d.ts
+++ env.d.ts
@@ -2,4 +2,7 @@
 // by running `wrangler types --env-interface CloudflareEnv env.d.ts`
 
 interface CloudflareEnv {
+    // If you set another name in wrangler.toml as the value for 'binding',
+    // replace "AI" with the variable name you defined.
+    AI: any;
 }
---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,26 @@
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+import type { NextRequest } from 'next/server';
+import { getRequestContext } from '@cloudflare/next-on-pages'
+
+import { Ai } from '@cloudflare/ai'
+
+// Set the runtime to edge for best performance
+export const runtime = 'edge';
+
+export async function POST(req: NextRequest) {
+    const { messages } = await req.json();
+
+    console.log(messages);
+
+    const ai = new Ai(getRequestContext().env.AI);
+
+    console.log("Create ai ok");
+
+    const stream = await ai.run("@cf/meta/llama-2-7b-chat-fp16", {
+        messages,
+        stream: true,
+    }) as ReadableStream;
+
+    // Respond with the stream
+    return new StreamingTextResponse(stream);
+}
