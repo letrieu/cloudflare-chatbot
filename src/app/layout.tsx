d871acb2eac12d48738ac7e975993a60770b1c0d
trieuld <trieuld@vng.com.vn>
2024-04-05 15:39:53 +0700

    Update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +16,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" data-theme="corporate">
+      <body className={inter.className}>
+
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,21 +5,31 @@ import { useChat } from 'ai/react';
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
   return (
-    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-      {messages.map(m => (
-        <div key={m.id} className={m.role === 'user' ? "chat chat-end" : "chat chat-start"}>
-          <div className={m.role === 'user' ? "chat-bubble chat-bubble-primary" : "chat-bubble chat-bubble-secondary"}> {m.content} </div>
+    <div>
+      <div className="flex flex-col h-dvh">
+        <div className="flex-grow h-0 overflow-y-auto">
+          <div className="flex flex-col w-full max-w-lg py-24 mx-auto">
+            {messages.map(m => (
+              <div key={m.id} className={m.role === 'user' ? "chat chat-end" : "chat chat-start"}>
+                <div className={m.role === 'user' ? "chat-bubble chat-bubble-primary" : "chat-bubble chat-bubble-secondary"}> {m.content} </div>
+              </div>
+            ))}
+          </div>
         </div>
-      ))}
-
-      <form onSubmit={handleSubmit}>
-        <input
-          className="input input-bordered input-primary fixed bottom-0 w-full max-w-md"
-          value={input}
-          placeholder="Say something..."
-          onChange={handleInputChange}
-        />
-      </form>
+        <footer className="footer footer-center p-4 text-base-content bg-base-300">
+          <form className='w-full' onSubmit={handleSubmit}>
+            <input
+              className="input input-bordered input-primary w-full max-w-lg"
+              value={input}
+              placeholder="Say something..."
+              onChange={handleInputChange}
+            />
+          </form>
+          <aside>
+            <p>Copyright © 2024 - AI Chatbot using Cloudflare AI Model and Vercel AI SDK developed by Trieu Le</p>
+          </aside>
+        </footer>
+      </div>
     </div>
   );
 }
